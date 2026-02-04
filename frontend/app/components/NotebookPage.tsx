f93f78af67a5374a9702fb1abb225cac5cd1ed1a
Wilesc447 <c.b.wiles@email.msmary.edu>
2026-02-04 15:36:52 -0500

    modified writing page to add basic word processing

---
+++ frontend/app/components/NotebookPage.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+const Editor = () => {
+    const editor = useEditor({
+        extensions: [StarterKit],
+        onUpdate: ({ editor }) => {
+            const html = editor.getHTML()
+        },
+        content: '<p>Start Typing...</p>',
+        injectCSS: false,
+        immediatelyRender: false,
+    })
+    return <EditorContent editor={editor} className="prose bg-base-300 border border-base-200 rounded-md focus:outline-none focus:border-accent w-screen" />
+}
+
+
+const NotebookPage = () => {
+    return (
+        <textarea className="bg-base-300 border border-base-300 rounded-md focus:outline-none focus:border-accent w-1/2" />
+    )
+}
+
+export default Editor
--- frontend/app/write/page.tsx
+++ frontend/app/write/page.tsx
@@ -1,12 +1,13 @@
 import ToolBar from "../components/ToolBar"
+import NotebookPage from "../components/NotebookPage"
 
 const Write = () => {
     return (
         <div>
             <ToolBar />
             {/* Page editor */}
-            <div className="flex justify-center h-screen pt-6" id="Document">
-                <textarea className="bg-base-300 border border-base-300 rounded-md focus:outline-none focus:border-accent w-1/2" placeholder="Start Writing..." />
+            <div className="flex justify-center h-screen pt-6">
+                <NotebookPage />
             </div>
         </div>
     )
